b53cdae82568cd2fdd0b4cd6db6d1fa0a32c80c8
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2022-03-02 13:46:59 +0200

    updated compulsory

--- Lab2/src/com/company/Main.java
+++ Lab2/src/com/company/Main.java
@@ -15,6 +15,16 @@ public class Main {
         Rooms: 401(cap=30, lab), 403(cap=30, lab), 405(cap=30, lab), 309(cap=100, lecture hall)
         */
 
-        // Room room1 = new Room("C302", "course", 100);
+        Event C1 = new Event("C1", 100,8, 10);
+        Event C2 = new Event("C2", 100, 10, 12);
+        Event L1 = new Event("L1", 30,8, 10);
+        Event L2 = new Event("L2", 30, 8, 10);
+        Event L3 = new Event("L3", 30, 10, 12);
+
+        Room room401 = new Room("401", "lab", 30);
+        Room room403 = new Room("403", "lab", 30);
+        Room room309 = new Room("309", "lecture_hall", 100);
+
+
     }
 }
